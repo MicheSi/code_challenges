f37fc41efa4358487b79ed8fba3565a589a76917
mlazzell17@gmail.com <MicheSi>
2020-08-31 12:47:40 -0700

    needle in a haystack

---
+++ code_challenges/needle_in_haystack.py
@@ -0,0 +1,19 @@
+# Implement strStr().
+
+# Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+# Example 1:
+
+# Input: haystack = "hello", needle = "ll"
+# Output: 2
+# Example 2:
+
+# Input: haystack = "aaaaa", needle = "bba"
+# Output: -1
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle in haystack:
+            return haystack.index(needle)
+        else:
+            return -1
